7a72e29e5baaac36310350479336c6748bc5e2d4
Embe <marcos96blount@gmail.com>
2023-07-14 22:59:19 -0500

    Update main.py with comments and add additional function for gathering configuration values

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
-from time import sleep
-import config
-import json
 import logging
+import json
 import requests
 import threading
-
-logging.basicConfig(level=logging.INFO)
+from time import sleep
+import config
 
 class HTTPClient:
     def __init__(self, base_url, cookie, authorization, user_agent):
+        """
+        Initializes an HTTP client with the necessary credentials and headers.
+
+        Args:
+            base_url (str): The base URL of the API.
+            cookie (str): The cookie value for authentication.
+            authorization (str): The authorization value for authentication.
+            user_agent (str): The user agent string for the request headers.
+        """
         self.base_url = base_url
         self.cookie = cookie
         self.authorization = authorization
         self.user_agent = user_agent
 
     def send_request(self, endpoint, body):
+        """
+        Sends an HTTP POST request to the specified endpoint with the given request body.
+
+        Args:
+            endpoint (str): The endpoint to send the request to.
+            body (dict): The request body in JSON format.
+
+        Returns:
+            requests.Response: The response object.
+
+        Raises:
+            requests.exceptions.RequestException: If an error occurs during the request.
+        """
         url = self.base_url + endpoint
         headers = {
             "Host": "germany-rental.taxify.eu",
@@ -39,10 +59,24 @@ class HTTPClient:
 
 class BoltScooter:
     def __init__(self, http_client, payment_instrument_id):
+        """
+        Initializes a BoltScooter instance with an HTTP client and payment instrument ID.
+
+        Args:
+            http_client (HTTPClient): The HTTP client to use for making requests.
+            payment_instrument_id (str): The payment instrument ID.
+        """
         self.http_client = http_client
         self.payment_instrument_id = payment_instrument_id
 
     def start_ride(self, vehicle_handle):
+        """
+        Starts a ride with the specified vehicle handle.
+
+        Args:
+            vehicle_handle (str): The ID of the Bolt scooter.
+
+        """
         create_and_start_body = {
             "vehicle_handle": {
                 "value": vehicle_handle,
@@ -52,6 +86,7 @@ class BoltScooter:
             "payment_instrument_id": self.payment_instrument_id
         }
 
+        # Send createAndStart request
         response = self.http_client.send_request("createAndStart", create_and_start_body)
         if response:
             logging.info("Response from createAndStart: %s", response.text)
@@ -66,17 +101,25 @@ class BoltScooter:
                 }
             }
 
+            # Send getActive request
             getactive_response = self.http_client.send_request("getActive", getactive_body)
             if getactive_response:
                 getactive_id_value = getactive_response.json().get('data', {}).get('order', {}).get('id')
 
+                # Start termination thread
                 termination_thread = threading.Thread(
-                    target=self.terminate_ride_after_delay, 
+                    target=self.terminate_ride_after_delay,
                     args=(getactive_id_value,)
                 )
                 termination_thread.start()
 
     def terminate_ride_after_delay(self, getactive_id_value):
+        """
+        Terminates the ride after a delay.
+
+        Args:
+            getactive_id_value (str): The ID of the active ride.
+        """
         sleep(50)  # Wait for 50 seconds before terminating the ride
 
         finish1_body = {
@@ -85,6 +128,7 @@ class BoltScooter:
             "gps_lng": "8.2257654130646571"
         }
 
+        # Send finish1 request
         finish1_response = self.http_client.send_request("finish", finish1_body)
         if finish1_response:
             logging.info("Finish1 request sent: Waiting for confirmation...")
@@ -96,22 +140,45 @@ class BoltScooter:
             "confirmed_view_keys": ["photo_capture_key"]
         }
 
+        # Send finish2 request
         finish2_response = self.http_client.send_request("finish", finish2_body)
         if finish2_response:
             logging.info("Finish2 request sent: Ride termination request completed successfully")
+    
+def get_configuration():
+    """
+    Retrieves the configuration values for the HTTP client and BoltScooter.
 
-def main():
+    Returns:
+        tuple: A tuple containing the configuration values (base_url, cookie, authorization,
+            payment_instrument_id, user_agent).
+    """
+    # Get configuration values from config module or any other source
     base_url = config.BASE_URL
     cookie = config.COOKIE
     authorization = config.AUTHORIZATION
     payment_instrument_id = config.PAYMENT_INSTRUMENT_ID
     user_agent = config.USER_AGENT
 
+    return base_url, cookie, authorization, payment_instrument_id, user_agent
+
+def main():
+    # Get the configuration values
+    base_url, cookie, authorization, payment_instrument_id, user_agent = get_configuration()
+
+    # Create an HTTP client and BoltScooter instance
     http_client = HTTPClient(base_url, cookie, authorization, user_agent)
     scooter = BoltScooter(http_client, payment_instrument_id)
 
+    # Prompt user for vehicle handle
     vehicle_handle = input("Please enter the id of your Bolt scooter (Placed under the scannable QR code): ")
+
+    # Start the ride
     scooter.start_ride(vehicle_handle)
 
 if __name__ == "__main__":
+    # Set up logging configuration
+    logging.basicConfig(level=logging.INFO)
+
+    # Run the main function
     main()
